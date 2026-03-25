30546e108fe8d84a792ad1d7aaf4b76899fccbaf
Tail-R <tr.wx58@gmail.com>
2026-03-25 23:57:16 +0900

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,84 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.colors = {
+    foreground = '#24292f',
+    background = '#fafafa',
+
+    cursor_fg = '#fafafa',
+    cursor_bg = '#24292f',
+
+    selection_fg = '#24292f',
+    selection_bg = '#e1e5ea',
+
+    scrollbar_thumb = '#4f5966',
+    -- split =
+
+    ansi = {
+        '#24292f',
+        '#b3202c',
+        '#4a915b',
+        '#b39020',
+        '#174d8f',
+        '#744d99',
+        '#157685',
+        '#d1d5da'
+    },
+    brights = {
+        '#4f5966',
+        '#cb2431',
+        '#519e63',
+        '#cca525',
+        '#1b5cab',
+        '#8759b3',
+        '#188899',
+        '#e1e5ea'
+    },
+
+    tab_bar = {
+        active_tab = {
+            fg_color = '#4f5966',
+            bg_color = '#fafafa'
+        },
+        inactive_tab = {
+            fg_color = '#4f5966',
+            bg_color = '#d1d5da'
+        },
+        new_tab = {
+            fg_color = '#4f5966',
+            bg_color = '#d1d5da'
+        }
+    }
+}
+
+config.window_padding = {
+    top = 0,
+    right = 10,
+    left = 0,
+    bottom = 0
+}
+
+config.use_fancy_tab_bar = true
+config.enable_tab_bar = true
+config.enable_scroll_bar = true
+config.enable_wayland = true
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = false
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = 'NeverPrompt'
+
+config.window_frame = {
+    font = wezterm.font { family = 'azukifontB', weight = 'Bold' },
+    font_size = 12,
+
+    active_titlebar_bg = '#e1e5ea',
+    inactive_titlebar_bg = '#e1e5ea'
+}
+
+config.window_background_opacity = 1.0
+
+config.font = wezterm.font('azukifontB', { weight = 'Regular', italic = false })
+config.font_size = 12
+
+return config
+
